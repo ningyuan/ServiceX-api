bae4eeea0c4bcc80887c6ae283f84b6d68664006
Ningyuan Pan <pan.neo@hotmail.com>
2020-04-25 15:22:04 +0800

    Integrate JPA data source manager

--- src/main/java/ningyuan/pan/servicex/util/GlobalObjectName.java
+++ src/main/java/ningyuan/pan/servicex/util/GlobalObjectName.java
@@ -10,5 +10,6 @@ package ningyuan.pan.servicex.util;
 public enum GlobalObjectName {
 	JDBC_DATA_SOURCE_MANAGER,
 	MYBATIS_DATA_SOURCE_MANAGER,
+	JPA_DATA_SOURCE_MANAGER,
 	JMS_DATA_SOURCE_MANAGER;
 }
